bb0972f46cbd4d2ed80266c78a973e7ca851861a
Nadeem Khan <nadeemkhanrtm@gmail.com>
2021-11-07 09:57:32 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState,useCallback} from 'react';
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  // console.log("a",window.scrollY)
+  const [y, setY] = useState(document.scrollingElement.scrollHeight);
+
+  const handleNavigation = useCallback(
+    (e) => {
+      
+      if (y > window.scrollY) {
+        console.log("scrolling up");
+      } else if (y < window.scrollY) {
+        console.log("scrolling down");
+      }
+      setY(window.scrollY)
+    },
+    [y]
   );
+
+  useEffect(() => {
+    // setY(window.scrollY);
+    document.body.scrollTop =  document.scrollingElement.scrollHeight;
+    console.log("scrollheight",document.scrollingElement.scrollHeight);
+    console.log('setted',y)
+    window.addEventListener("scroll", handleNavigation);
+
+    return () => {
+      window.removeEventListener("scroll", handleNavigation);
+    };
+  }, [handleNavigation]);
+
+ const handleClick = () => {
+  document.documentElement.scrollTop = 100;
+  }
+  return (
+    <section className={styles.main_container}>
+      <h1>Top</h1>
+      <h2>Middle</h2>
+      <h3 onClick={handleClick}>End</h3>
+    </section>
+  )
 }
 
-export default App;
+export default App
